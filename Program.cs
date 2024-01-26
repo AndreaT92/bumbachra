77a8cb84330e8ae8976d1f1d1b1ce31d5d85bc93
Andrea Tauberová <andrea.tauberova@seznam.cz>
2024-01-26 15:39:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace bumbac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int vypsaneCislo = 1; vypsaneCislo <= 100; vypsaneCislo++)
+            {
+                if (vypsaneCislo % 5 == 0 && vypsaneCislo % 3 == 0)
+                {
+                    Console.WriteLine("BumBac");
+                }
+                else if (vypsaneCislo % 3 == 0)
+                {
+                    Console.WriteLine("Bum");
+                }
+                else if (vypsaneCislo % 5 == 0)
+                {
+                    Console.WriteLine("Bac");
+                }
+                else
+                {
+                    Console.WriteLine(vypsaneCislo);
+                }
+            }
+        }
+    }
+}
